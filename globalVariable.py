8a7877a4b930659b1b1f7fb7f19e51b461d0c0c6
Karim <karimhamid@yandex.ru>
2020-05-04 19:15:54 +0300

    first commit

---
+++ globalVariable.py
@@ -0,0 +1,14 @@
+""" Файл для хранения глобальных переменных файла main.py """
+
+
+QUERY_COMMANDS = {
+    'check_username': "select count(username) from users where username='%s';",
+    'add_user': "insert into users (username, hashed_password) values ('%s', '%s');",
+    'get_password': "select hashed_password from users where username='%s';"
+}  # TODO: Дописать все команды
+
+
+PASSWORD_FORM = '''Password form: 
+1) Use only latin character, numbers and symbol '_' 
+2) Max length - 15
+3) Min length - 5'''
---
+++ main.py
@@ -0,0 +1,100 @@
+from flask import Flask, request, g, abort, render_template, url_for, redirect, make_response
+from globalVariable import *
+from app import app
+import bcrypt
+import re
+import mysql.connector
+
+
+
+
+db_cursor = data_base.cursor()
+
+
+def verify_password_on_correct(**verifiable):
+    """ dict([str key: str element],) -> dict([str key: bool],)
+        Проверяет, нет ли в строке лишних символов.
+        В конце возвращает словарь, где каждому ключу соответвует занчение,
+        подходит ли строка или нет.
+    """
+    result = dict()
+    expression = re.compile(r'[a-zA-Z0-9_]{5,15}$')
+    for key, element in verifiable.items():
+        if expression.match(element):
+            result[key] = True
+        else:
+            result[key] = False
+    return result
+
+
+def accessing_the_database(query, changes=False):
+    db_cursor.execute(query)
+    if changes:
+        data_base.commit()
+        return None
+    return db_cursor.fetchall()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', session=True)
+
+
+@app.route('/login', methods=['post', 'get'])
+def login():
+    message = ''  # Для вывода предупреждений в форме
+    if request.method == 'POST':
+        username = str(request.form.get('username'))
+        password = str(request.form.get('password'))
+        if not username and password:
+            message = 'Empty login or password.'
+        elif accessing_the_database(QUERY_COMMANDS['check_username'] % username)[0][0] == 0:  # count(username)
+            message = 'User does not exist.'
+        elif not all(verify_password_on_correct(username=username, password=password).values()):
+            message = PASSWORD_FORM
+        else:
+            result = accessing_the_database(QUERY_COMMANDS['get_password'] % username)
+            if bcrypt.checkpw(bytes(password, encoding='utf-8'), result[0][0]):
+                return redirect(url_for('index'))  # TODO: Генерировать тут сессию
+            else:
+                message = 'Wrong password.'
+    return render_template('login.html', message=message, session=True)
+
+
+@app.route('/registration', methods=['post', 'get'])
+def registration():
+    message = ''  # Для вывода предупреждений в форме
+    if request.method == 'POST':
+        username = str(request.form.get('username'))
+        password = str(request.form.get('password'))
+        rep_password = str(request.form.get('rep_password'))
+        if not username and password and rep_password:
+            message = 'Empty login or one of passwords.'
+        elif accessing_the_database(QUERY_COMMANDS['check_username'] % username)[0][0] != 0:  # count(username)
+            message = 'User with the same name already exist.'
+        elif not all(verify_password_on_correct(
+                username=username, password=password,
+                rep_password=rep_password).values()):
+            message = PASSWORD_FORM
+        elif rep_password != password:
+            message = 'Passwords are not equal.'
+        else:
+            accessing_the_database(QUERY_COMMANDS['add_user'] \
+            % (username, bcrypt.hashpw(bytes(password, encoding='utf-8'),
+                        bcrypt.gensalt()).decode('utf-8')), changes=True)
+            return redirect(url_for('index'))  # TODO: Генерировать тут сессию
+    return render_template('registration.html', message=message, session=True)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', session=True)
+
+
+@app.route('/about-us')
+def about_us():
+    return render_template('about-us.html', session=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
