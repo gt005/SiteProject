bb3be0b9f35b86c31a5f7b3b31564087bfc80e21
Karim <karimhamid@yandex.ru>
2020-05-10 15:28:40 +0300

    Last

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from datetime import timedelta
-from globalVariable import *
-
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = SECRET_KEY
-    app.permanent_session_lifetime = timedelta(days=7)
-    return app
